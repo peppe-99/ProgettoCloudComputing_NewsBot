74ab7d80b46650d453e3bdebf14e832332572729
peppe-99 <g.cardaropoli99@gmail.com>
2022-01-10 16:15:12 +0100

    add macro to invoke LUIS

--- NewsBot/config.py
+++ NewsBot/config.py
@@ -8,5 +8,10 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
+    APP_TYPE = os.environ.get("MicrosoftAppType", "")
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
+    NEWS_BY_SUBJECT_FUNCTION_URL = os.environ.get("NewsBySubjectFunctionURL", "")
