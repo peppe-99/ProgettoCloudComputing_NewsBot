bda7c314834979c8062e7b4b876935967d089344
peppe-99 <g.cardaropoli99@gmail.com>
2022-01-21 11:21:02 +0100

    config file

--- Functions/config.py
+++ Functions/config.py
@@ -2,8 +2,8 @@ import os
 
 class DefaultConfig:
 
-    COSMOS_DB_URL = os.environ.get("CosmosDBURL", "https://cosmos-db-news-bot.documents.azure.com:443/")
-    COSMOS_DB_KEY = os.environ.get("CosmosDBKey", "Kls96q9eAvM13SzxaSObVtqQrK2xegVYyULdvmc1ai3YWAjLVIzHJvFx3rJCIhcqviPwBrKtKo8ImgkVqEl06A==")
-    BING_KEY = os.environ.get("BingKey", "66c387861a144d088af9b5b6ccb5c613")
-    SEARCH_NEWS_URL = os.environ("SearchNewsURL", "https://api.bing.microsoft.com/v7.0/news/")
-    SEARCH_URL = os.environ("SearchURL", "https://api.bing.microsoft.com/v7.0/news/search")
+    COSMOS_DB_URL = os.environ.get("CosmosDBURL", "")
+    COSMOS_DB_KEY = os.environ.get("CosmosDBKey", "")
+    BING_KEY = os.environ.get("BingKey", "")
+    SEARCH_NEWS_URL = os.environ.get("SearchNewsURL", "")
+    SEARCH_URL = os.environ.get("SearchURL", "")
